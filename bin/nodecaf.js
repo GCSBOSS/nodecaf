c9236a7fb715e402f11a8c0084db7326478b86c5
Guilherme C. Souza <gcsgacc@gmail.com>
2019-06-11 23:21:23 -0300

    Add options for conf file generation with cli init

--- bin/nodecaf.js
+++ bin/nodecaf.js
@@ -3,7 +3,10 @@ const cli = require('cli');
 
 const COMMANDS = [ 'init' ];
 
-let opts = cli.parse({}, COMMANDS);
+let opts = cli.parse({
+    confPath: [ 'c', 'Conf file path', 'file', undefined ],
+    confType: [ false, 'Conf file extension', 'as-is', undefined ]
+}, COMMANDS);
 let cmd = require('../lib/cli/' + cli.command);
 try{
     cmd(opts);
