eaca60b2067d68bf54ce0fcd9e4bd1c75ad9ece2
Guilherme C. Souza <gcsgacc@gmail.com>
2019-07-15 17:42:57 -0300

    Remove built-in version and help commands

--- bin/nodecaf.js
+++ bin/nodecaf.js
@@ -2,11 +2,6 @@
 const fs = require('fs');
 const path = require('path');
 
-if(process.argv[2] == '-h')
-    process.argv[2] = 'help';
-else if(process.argv[2] == '-v')
-    process.argv[2] = 'version';
-
 let cf = path.resolve(__dirname, '../lib/cli', process.argv[2] + '.js');
 
 if(fs.existsSync(cf)){
